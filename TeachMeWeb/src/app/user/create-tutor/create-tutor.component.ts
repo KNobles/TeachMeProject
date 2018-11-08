e2b401de151035f8d627dd6de4986b4c38d13379
KNobles <nobles-k@hotmail.com>
2018-11-08 17:07:33 +0100

    created components

---
+++ TeachMeWeb/src/app/user/create-tutor/create-tutor.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-create-tutor',
+  templateUrl: './create-tutor.component.html',
+  styleUrls: ['./create-tutor.component.css']
+})
+export class CreateTutorComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
