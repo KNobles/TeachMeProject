3623ccd7ff30f7f4b679ce93b0bf6921d6f85565
la163004 <la163004@student.helha.be>
2018-11-10 09:47:40 +0100

    Ajout des formulaire dans le html

--- TeachMeWeb/src/app/user/create-user/create-user.component.ts
+++ TeachMeWeb/src/app/user/create-user/create-user.component.ts
@@ -1,5 +1,5 @@
 import {Component, EventEmitter, OnInit} from '@angular/core';
-import {Eleve} from "../eleve";
+import {Eleve} from '../../eleve/eleve';
 
 @Component({
   selector: 'app-create-user',
@@ -8,11 +8,18 @@ import {Eleve} from "../eleve";
 })
 export class CreateUserComponent implements OnInit {
   private _eleveTmp: Eleve = new Eleve;
-  private _eleveCreated:EventEmitter<Eleve> = new EventEmitter();
+  private _eleveCreated: EventEmitter<Eleve> = new EventEmitter();
+  private _isHidden: boolean;
 
   constructor() { }
 
   ngOnInit() {
+  }
+  TypeFormulaire () {
+    this._isHidden = ! this._isHidden;
+  }
+  get isHidden(): boolean {
+    return this._isHidden;
   }
 
   get eleveTmp(): Eleve {
