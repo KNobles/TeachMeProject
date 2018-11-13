0bcdd1b06cecc0e09a687a4e62c13b7d83584135
la163004 <la163004@student.helha.be>
2018-11-13 15:27:24 +0100

    Ajout getter et setter dan login et Create personnne

--- TeachMeWeb/src/app/login/login.component.ts
+++ TeachMeWeb/src/app/login/login.component.ts
@@ -7,11 +7,27 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
   private _login: string ;
-  private _mdp: string ;
+  private _password: string ;
   constructor() {}
   ngOnInit() {
   }
   Connection() {
-    alert(this._login + ' ' + this._mdp);
+    alert(this._login + ' ' + this._password);
+  }
+
+  get login(): string {
+    return this._login;
+  }
+
+  set login(value: string) {
+    this._login = value;
+  }
+
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
   }
 }
--- TeachMeWeb/src/app/user/create-personne/create-personne.component.ts
+++ TeachMeWeb/src/app/user/create-personne/create-personne.component.ts
@@ -7,10 +7,46 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./create-personne.component.css']
 })
 export class CreatePersonneComponent implements OnInit {
+    private _userName: string;
+    private _password: string;
+    private _mail: string;
+    private _telNumber: string;
 
   constructor() { }
 
   ngOnInit() {
   }
+  Test() {
+    alert(this._userName + ' ' + this._password + ' ' + this._mail + ' ' + this._telNumber);
+  }
+  get userName(): string {
+    return this._userName;
+  }
+  set userName(value: string) {
+    this._userName = value;
+  }
 
+  get password(): string {
+    return this._password;
+  }
+
+  set password(value: string) {
+    this._password = value;
+  }
+
+  get mail(): string {
+    return this._mail;
+  }
+
+  set mail(value: string) {
+    this._mail = value;
+  }
+
+  get telNumber(): string {
+    return this._telNumber;
+  }
+
+  set telNumber(value: string) {
+    this._telNumber = value;
+  }
 }
