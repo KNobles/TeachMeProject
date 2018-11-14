8611e55bb1155fc6ad06cd9522d570d7494b81f3
la163004 <la163004@student.helha.be>
2018-11-14 11:43:09 +0100

    Tentative de pull

--- TeachMeWeb/src/app/app-routing.module.ts
+++ TeachMeWeb/src/app/app-routing.module.ts
@@ -1,7 +1,6 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import {LoginComponent} from './login/login.component';
-
 import {CreateUserComponent} from './user/create-user/create-user.component';
 
 const routes: Routes = [
