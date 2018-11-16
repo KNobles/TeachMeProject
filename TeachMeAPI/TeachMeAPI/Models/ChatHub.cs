c854ca45ca3df71776dacfbeb17b9c898ccc615e
KNobles <nobles-k@hotmail.com>
2018-11-16 22:24:06 +0100

    Made websocket

---
+++ TeachMeAPI/TeachMeAPI/Models/ChatHub.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Web;
+using Microsoft.AspNet.SignalR;
+namespace TeachMeAPI
+{
+    public class ChatHub : Hub
+    {
+        public void Send(string name, string message)
+        {
+            // Call the broadcastMessage method to update clients.
+            Clients.All.broadcastMessage(name, message);
+        }
+
+        public void CreateAnnouncemement(string username, string title, string course, string description)
+        {
+            Clients.All.broadcastAnnouncement(username, title, course, description);
+        }
+    }
+}
